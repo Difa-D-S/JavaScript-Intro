d2441c3366f0ca922f5b16d2f5c9d8ec462c35a7
Difa-D-S <difads123@gmail.com>
2024-02-22 10:07:54 +0530

    Basic JavaScript

---
+++ assignment.js
@@ -0,0 +1,185 @@
+// Values and Variables
+
+    // var country = "India";
+    // var continent = "Asia";
+    // var population = 4;
+
+    // console.log(country, continent, population + " million");
+
+// Data Types
+
+    // var isIsLand = true;
+    // var language;
+
+    // console.log(typeof isIsLand);
+    // console.log(typeof population);
+    // console.log(typeof country);
+    // console.log(typeof language);
+
+// let, const and var
+
+    // var language = "English";
+    //  language = "Tamil";
+
+    // console.log(language);
+    
+// Basic Operators
+    // var population = 10000/2;
+    // var country = 10/2;
+    // console.log(country, population);
+    
+
+// if-else Statement
+
+    // var population = 130;
+    // var average = 33;
+    // var aver = average - population;
+    // if(population > average){
+    //     console.log("Portugal's population is above average")
+    // }
+    // else{                                   // Template Literals
+    //     console.log(`Portugals's population is ${aver} million below average`);
+    // }
+
+// // const inputyear = "200";
+// // console.log(Number(inputyear), inputyear);
+// // console.log(inputyear + 18);
+
+
+// Type Conversion and Coercion
+
+    // console.log('9' - '5');
+
+    // console.log('19' - '13' + '17');
+    // console.log('19' - '13' + 17);
+
+    // console.log('123' < 57);
+    // console.log(5 + 6 + '4' + 9 - 4 - 2);
+
+
+// Switch Case
+
+// var language = "spanish";
+// switch(language){
+//     case "chinese":
+//         console.log("MOST number of native speakers!");
+//         break;
+//     case "mandarin":
+//         console.log("MOST number of native speakers!");
+//         break;
+//     case "spanish" :
+//         console.log("2nd place in number of native speakers");
+//         break;
+//     case "english" :
+//         console.log("3rd place");
+//         break;
+//     case "hindi" :
+//         console.log("Number 4");
+//         break;
+//     case "arabic" :
+//         console.log("5th most spoken language");
+//         break;
+//     default :
+//         console.log("Great language too");
+//         break;
+// }
+
+
+// Conditional (Ternary) Operator
+
+    // let population = 13;
+    // const average = 33;
+    // let averagePopulation = average - population;
+
+    //             //  population > average
+    //             //    ? console.log("Portugal's population is above average")
+    //             //    : console.log("Portugal's population is below average")
+
+    //                 //// OR
+
+    //     console.log(`Portugal's population is ${population > average ? "above" : "below" } average`);
+
+
+// Equality == vs ===    // == perform type coercion if 1 is string it will execute only 1 border!
+                        // === not perform type coercion if 1 is string it will not convert 1 to number and result will be no borders.
+                       // Number() we can use == as well as ===
+
+    // var numNeighbours = Number(prompt('How many neighbour countries does your country have?'));
+    // console.log(numNeighbours, typeof numNeighbours);
+    // if(numNeighbours === 1)      
+    // {
+    //     console.log("Only 1 border!");
+    // }
+    // else if(numNeighbours > 1){
+    //     console.log(`${numNeighbours} is greater than 1`);
+    // }
+    // else{
+    //     console.log("No borders");           //prompt and alert
+    // }
+
+
+
+// Basic Operators
+
+    // var population =10;
+    // var averagePopulation = population/2;
+
+    // var country = "Portugal";
+
+    // console.log(averagePopulation);
+
+    // population++;
+    // console.log(population);
+
+    // var finLand = 6;
+    // if(population > finLand){
+    //     console.log("Country have more people than finLand");
+    // }
+    // else{
+    //     console.log("Country have less people than finLand");
+    // }
+
+    // var avgPopulation = 33;
+    // if(population < avgPopulation){
+    //     console.log("Population is less");
+    // }
+    // else{
+    //     console.log("Population is high");
+    // }
+
+    // var description = country + " is in Europe, and its " + population + " million people speak Portuguese";
+    // console.log(description);
+
+
+
+// Template Literal
+
+    // var description = `${country} is in Europe, and its ${population} million people speak Portuguese`;
+    // console.log(description);
+
+
+
+
+// Logical Operators
+
+    // speakEnglish = true;
+    // var population = 55_00_000;
+    // populationLimit = 50_00_000;
+    // isLand = false;
+
+    // if(speakEnglish && populationLimit < population && isLand){
+    //     console.log("You should live in Portugal :)")
+    // }
+    // else{
+    //     console.log("Portugal does not meet your criteria :(")
+    // }
+
+
+
+
+
+
+
+
+
+
---
+++ script.js
@@ -0,0 +1,170 @@
+// // console.log(5+7);
+// // var fname=9.08;
+// // console.log(fname);
+
+// // Function declaration
+// function Operator(){
+//     var a=5;
+//     var b=7;
+//     console.log(a+b);
+//     console.log(a-b);
+//     console.log(a*b);
+//     console.log(a/b);
+// }
+
+// // Function Calling
+// Operator();
+
+// // Arguments
+// function Sum(a,b,c){
+
+//     console.log(a+b+c);
+// }
+
+
+// // function argument calling
+// Sum(7,3,5);
+
+// function Multiply(a,b){
+//     console.log(a*b);
+// }
+
+
+// Multiply(10,10);
+
+
+// //Arrow Function
+// const Add =(a,b,c) => {
+//     console.log(a+b+c);
+// }
+
+// // arrow function calling
+
+// Add(10,20,30);
+
+
+
+// Condition
+
+// var time = 30;
+// if(time < 12){
+//     console.log("Good Morning");
+// }
+// else if(time > 12 &&  time < 17){
+//     console.log("Good afternoon");
+// }
+
+// else if(time > 17 && time < 24){
+//     console.log("Good Night");
+// }
+
+// else{
+//     console.log("No Result");
+// }
+
+
+// if-else Statement
+
+// var number1 = 14;
+// var number2 = 60;
+// var number3 = 34;
+// if(number1 > number2 && number1 > number3){
+//     console.log(" number1 is bigger");
+// }
+// else if(number2 > number3 && number2 > number1){
+//     console.log(" number2 is bigger");
+// }
+// else{
+//     console.log(" number3 is bigger");
+// }
+
+
+// Switch Statement
+
+// var a=9;
+
+// switch(a){
+//     case 1:
+//         console.log("Monday");
+//         break;
+//     case 2:
+//         console.log("Tuesday");
+//         break;
+//     case 3:
+//         console.log("Wednesday");
+//         break;
+//     case 4:
+//         console.log("Thursday");
+//         break;
+//     case 5:
+//         console.log("Friday");
+//         break;
+//     case 6:
+//         console.log("Saturday");
+//         break;
+//     case 7:
+//         console.log("Sunday");
+//         break;
+//     default:
+//         console.log("No Days");
+//         break;
+// }
+
+
+// Looping Statement
+
+// For loop we must want to give "let".
+
+    
+
+    // for(let i=5; i<=25; i++){
+    //     console.log(i);
+    // }
+
+    // while Loop
+
+    // var i=15;
+    // while(i<=25){
+    //     console.log(i);
+    //     i++;
+    // }
+
+    // For of Loop
+
+    // var numbers = [11, 21, 31, 41, 51];
+
+    // console.log(numbers[2]);
+
+    // var names = ["Difa", "Namitha", 4, 8, "Varsha", true, false];
+
+    // console.log(names[6]);
+
+    // for(var i of names){
+    //     console.log(i);
+    // }
+
+    // for(var i in names){
+    //     console.log(i);
+    // }
+
+
+// String Methods (lenght. charcode, chatAt)
+
+// var text = "Hello World";        
+// console.log(text.length);
+
+// // Array Methods
+
+// var clg=["NMCC", "Maria"];
+// console.log(clg.pop());
+
+// var clg=["NMCC", "Maria"];
+// const sli=clg.slice(1);
+// console.log(sli);
+
+
+
+// Date Object
+
+
+
